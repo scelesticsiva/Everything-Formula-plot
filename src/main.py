18eb209cd6049ab99f9c460de5be755f0bd6c198
Sivaramakrishnan Sankarapandian <scelesticsiva@gmail.com>
2018-06-29 21:31:12 -0400

    added bitmap and character parsing

--- src/main.py
+++ src/main.py
@@ -2,4 +2,19 @@
 @author - Sivaramakrishnan
 """
 import numpy as np
+from src import utils
+import argparse
 
+def save_graph_and_k(inp_string,save_fig = False):
+    grid = utils.char_to_bitmap(inp_string)
+    k = utils.find_k(grid)
+    utils.plot_grid(grid,k,save_fig)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--string",type = str,\
+                        help = "String to which k value has to be found")
+    parser.add_argument("--save",type = bool,\
+                        help = "Whether to save the figure")
+    args = parser.parse_args()
+    save_graph_and_k(args.string,args.save)
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@
 """
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
 def find_k(grid):
     grid_flattened = grid.flatten("F").tolist()
@@ -21,6 +22,36 @@ def create_graph_with_k(k):
             dummy_grid[i][j] = int(not formula_result(j,k+i))
     return np.flip(np.flip(dummy_grid,axis = 1),axis = 0)
 
-def plot_grid(result_grid):
+def break_string_of_k(k,break_point = 80):
+    string_k = str(k)
+    return_string = ""
+    len_string,i = len(string_k),0
+    print("len:",len_string)
+    print(string_k)
+    while(i < len_string//break_point):
+        return_string += string_k[i*break_point:(i+1)*break_point]+"\n"
+        i += 1
+    return_string += string_k[i*break_point:]
+    return return_string
+
+def char_to_bitmap(inp_string,JSON_FILE = "char_bitmap_5x5.json"):
+    string = inp_string.lower()
+    new_grid = np.zeros((17,106),dtype = np.int32)
+    index = 1
+    with open(JSON_FILE) as json_file:
+        char_to_bitmap_dict = json.load(json_file)
+    for each_letter in string:
+        temp = np.array(char_to_bitmap_dict[each_letter],dtype = np.int32)
+        height,width = temp.shape
+        reshaped_temp = np.zeros((17,width+1))
+        reshaped_temp[(17//2 - height//2):((17//2 - height//2)+height),0:-1] = temp
+        new_grid[:,index:index+width+1] = reshaped_temp
+        index += (width + 1)
+    return new_grid
+
+def plot_grid(result_grid,k,save = False):
     plt.imshow(result_grid,cmap="binary")
+    plt.xlabel("k = "+break_string_of_k(k),fontsize = 8)
+    if save:
+        plt.imsave("grid.png")
     plt.show()
